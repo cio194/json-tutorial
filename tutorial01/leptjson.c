5e4f186ae9c676a37a3f48acdf081304faf3262a
cio <194384095@qq.com>
2021-04-06 16:46:16 +0800

    tutorial5 resolve array

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -300,6 +300,65 @@ static int lept_parse_string(lept_context* c, lept_value* v)
 	}
 }
 
+static int lept_parse_value(lept_context* c, lept_value* v); /* 前向声明 */
+
+static int lept_parse_array(lept_context* c, lept_value* v)
+{
+	size_t size = 0;
+	int ret;
+
+	EXPECT(c, '[');
+	lept_parse_whitespace(c);
+	if (*c->json == ']')
+	{
+		c->json++;
+		v->u.a.e = NULL;
+		v->u.a.size = 0;
+		v->type = LEPT_ARRAY;
+		return LEPT_PARSE_OK;
+	}
+
+	while (1)
+	{
+		lept_value e; // 临时结点
+		lept_init(&e);
+		if ((ret = lept_parse_value(c, &e)) != LEPT_PARSE_OK)
+			return ret;
+		size++;
+		memcpy(lept_context_push(c, sizeof(lept_value)), &e, sizeof(lept_value));
+		// lept_free(&e); // 释放为临时lept_value e分配的内存空间
+		// 然而不需要这个，因为memcpy实际上copy的是lept_value，而lept_value中放的都是指针
+		// 如果调用了，才会出问题呢，不调用才是正确的
+
+		lept_parse_whitespace(c);
+		if (*c->json == ',')
+		{
+			c->json++;
+			lept_parse_whitespace(c);
+			continue;
+		}
+		else if (*c->json == ']')
+		{
+			c->json++;
+			v->u.a.e = (lept_value*)malloc(size * sizeof(lept_value));
+			memcpy(v->u.a.e, (lept_value*)lept_context_pop(c, size * sizeof(lept_value)), size * sizeof(lept_value));
+			v->u.a.size = size;
+			v->type = LEPT_ARRAY;
+			return LEPT_PARSE_OK;
+		}
+		else
+		{
+			ret = LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+			break;
+		}
+	}
+
+	/* Pop and free values on the stack */
+	for (int i = 0; i < size; i++)
+		lept_free((lept_value*)lept_context_pop(c, sizeof(lept_value)));
+	return ret;
+}
+
 // 感觉类似编译器的词法分析器； 状态转换
 static int lept_parse_value(lept_context* c, lept_value* v)
 {
@@ -309,6 +368,7 @@ static int lept_parse_value(lept_context* c, lept_value* v)
 	case 't': return lept_parse_literal(c, v, "true", LEPT_TRUE);
 	case 'f': return lept_parse_literal(c, v, "false", LEPT_FALSE);
 	case '"': return lept_parse_string(c, v);
+	case '[': return lept_parse_array(c, v);
 	default: return lept_parse_number(c, v);
 	case '\0': return LEPT_PARSE_EXPECT_VALUE;
 	// default: return LEPT_PARSE_INVALID_VALUE;
@@ -347,6 +407,15 @@ void lept_free(lept_value* v)
 	assert(v != NULL);
 	if (v->type == LEPT_STRING)
 		free(v->u.s.s);
+	else if (v->type == LEPT_ARRAY)
+	{
+		size_t size = v->u.a.size;
+		for (int i = 0; i < size; i++)
+		{
+			lept_free(v->u.a.e + i);
+		}
+		free(v->u.a.e);
+	}
 	v->type = LEPT_NULL;
 }
 
@@ -404,4 +473,18 @@ void lept_set_string(lept_value* v, const char* s, size_t len)
 	v->u.s.s[len] = '\0';
 	v->u.s.len = len;
 	v->type = LEPT_STRING;
+}
+
+size_t lept_get_array_size(const lept_value* v)
+{
+	assert(v != NULL && v->type == LEPT_ARRAY);
+	return v->u.a.size;
+}
+
+lept_value* lept_get_array_element(const lept_value* v, size_t index)
+{
+	assert(v != NULL && v->type == LEPT_ARRAY);
+	assert(index >= 0 && index < v->u.a.size);
+	// return v->u.a.e + index;
+	return &(v->u.a.e[index]);
 }
